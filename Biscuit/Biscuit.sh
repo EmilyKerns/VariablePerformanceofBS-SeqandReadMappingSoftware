2e22a28e20e34830b8304491c2f3decedc33bc11
EmilyKerns <116760986+EmilyKerns@users.noreply.github.com>
2026-01-28 11:48:48 -0600

    Upload Biscuit executable file

---
+++ Biscuit/Biscuit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PATH=/opt/conda/bin:$PATH
+
+# prior to running this script, move the indexed genome files to one folder and create a tar.gz 
+tar -xvzf biscuit_ref.tar.gz --strip-components=1
+
+fq=$1
+
+echo "Aligning WGBS reads with Biscuit on $(hostname) at $(date)"
+echo "  Processing sample: ${fq}"
+echo "  Running Biscuit with 16 threads..."
+
+# Replace GCF_013753865.1_Amil_v2.1_genomic.fasta with your reference genome and adjust -@ 16 to however many threads you want to use for alignment
+biscuit align -@ 16 GCF_013753865.1_Amil_v2.1_genomic.fasta ${fq}_1_val_1.fq.gz ${fq}_2_val_2.fq.gz |
+        /opt/conda/bin/samtools sort -o ${fq}_biscuit.bam -O BAM
+
+
+echo "Read alignment completed at $(date)"
