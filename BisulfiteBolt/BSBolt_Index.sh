5a4e9540f4e11eb8ec19d9949aa2908382729bcc
EmilyKerns <116760986+EmilyKerns@users.noreply.github.com>
2026-01-28 12:44:28 -0600

    Upload BiSulfite Bolt script for indexing the reference

---
+++ BisulfiteBolt/BSBolt_Index.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Replace REFERENCE with your reference genome and /PATH/BSBolt with the directory you want to store the indexed reference genome in
+bsbolt Index -G REFERENCE -DB /PATH/BSBolt
