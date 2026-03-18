7e91ed209d5a3c0967caa7a6abaf429faa7fc618
EmilyKerns <116760986+EmilyKerns@users.noreply.github.com>
2026-03-18 12:41:21 -0500

    Update comment to include compression note

--- BisulfiteBolt/BSBolt_Index.sh
+++ BisulfiteBolt/BSBolt_Index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Replace REFERENCE with your reference genome and /PATH/BSBolt with the directory you want to store the indexed reference genome in
+# Replace REFERENCE with your reference genome and /PATH/BSBolt with the directory you want to store the indexed reference genome in. Can compress the reference into tar.gz before running alignment script
 bsbolt Index -G REFERENCE -DB /PATH/BSBolt
